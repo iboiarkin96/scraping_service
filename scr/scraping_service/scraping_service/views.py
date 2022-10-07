87b3edd90b5c4c639376d124409dc086a521921a
= <=>
2022-10-07 19:59:47 +1000

    second commit

--- scr/scraping_service/scraping_service/views.py
+++ scr/scraping_service/scraping_service/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 import datetime
 
 def home(request):
+    'Функция с подстановкой'
     date = datetime.datetime.now().date
     name = 'Dave'
     _context = {'date' : date, 'name' : name}
