356a75d2c196314c12f897c47c543494c38b9e1a
= <=>
2022-10-15 16:50:09 +1000

    add form redaction

--- src/scraping_service/scraping/forms.py
+++ src/scraping_service/scraping/forms.py
@@ -1,22 +1,26 @@
 from django import forms
 from .models import Language, City
 
+
 class FindForm(forms.Form):
     """Основная форма поиска на сайте
     """
-    city = forms.ModelChoiceField(queryset= City.objects.all(),
-                                    empty_label='Введите город', 
-                                    to_field_name='name', 
-                                    required=False,
-                                    widget = forms.Select(attrs = {'class' : 'form-control'}), 
-                                    label='Город'
-                                   
-                                    )
+    city = forms.ModelChoiceField(
+        queryset=City.objects.all(),
+        empty_label='Введите город',
+        to_field_name='name',
+        required=False,
+        widget=forms.Select(
+            attrs={
+                'class': 'form-control'}),
+        label='Город')
 
-    Language = forms.ModelChoiceField(queryset= Language.objects.all()
-                                        , empty_label='Введите специальность'
-                                        , to_field_name='name'
-                                        , required=False
-                                        , widget = forms.Select(attrs = {'class' : 'form-control'}),
-                                        label='Специальность'
-                                     )
+    Language = forms.ModelChoiceField(
+        queryset=Language.objects.all(),
+        empty_label='Введите специальность',
+        to_field_name='name',
+        required=False,
+        widget=forms.Select(
+            attrs={
+                'class': 'form-control'}),
+        label='Специальность')
--- src/scraping_service/scraping/views.py
+++ src/scraping_service/scraping/views.py
@@ -2,11 +2,12 @@ from .forms import FindForm
 from django.shortcuts import render
 from .models import Vacancy
 
+
 def home_view(request):
-    #  данныепараметры пришли из формы на сайте 
+    #  данныепараметры пришли из формы на сайте
     city = request.GET.get('city')
     language = request.GET.get('language')
-    
+
     qs = []
     _filter = {}
     form = FindForm()
@@ -17,5 +18,5 @@ def home_view(request):
         if language:
             _filter['language__name'] = language
         qs = Vacancy.objects.filter(**_filter)
-    _context = dict(object_list = qs, form = form)
-    return render(request, 'scraping/home.html', context = _context)
+    _context = dict(object_list=qs, form=form)
+    return render(request, 'scraping/home.html', context=_context)
